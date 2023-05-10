6e8ff5d0c736c423b859729ff81cedff3039157a
jlbel <julia.belyaeva2012@gmail.com>
2023-05-10 17:50:51 +0300

    Добавьте файлы проекта.

---
+++ Comics/PriceRange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comics
+{
+    public enum PriceRange
+    {
+        Cheap,
+        Expensive,
+    }
+}
